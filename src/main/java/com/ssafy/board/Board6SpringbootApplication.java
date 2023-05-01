b9d4a9d6ff192adfebb2b6090ae82bc50147fd69
김영서 <kys981207@naver.com>
2023-05-01 13:45:56 +0900

    first commit

---
+++ src/main/java/com/ssafy/board/Board6SpringbootApplication.java
@@ -0,0 +1,16 @@
+package com.ssafy.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.ssafy"})
+public class Board6SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Board6SpringbootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ssafy/config/WebMvcConfiguration.java
@@ -0,0 +1,52 @@
+package com.ssafy.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.ssafy.interceptor.ConfirmInterceptor;
+
+@Configuration
+@MapperScan(basePackages = {"com.ssafy.**.mapper"})
+public class WebMvcConfiguration implements WebMvcConfigurer {
+	
+	private final List<String> patterns = Arrays.asList("/article/*", "/admin", "/user/list");
+	
+	private ConfirmInterceptor confirmInterceptor;
+	
+	@Value("${file.path")
+	private String uploadPath;
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+	}
+
+	public WebMvcConfiguration(ConfirmInterceptor confirmInterceptor) {
+		this.confirmInterceptor = confirmInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(confirmInterceptor).addPathPatterns(patterns);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index2");
+	}
+
+}
