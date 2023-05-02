86a081a5780e04301d1fb2ac4b76a294853930a6
김영서 <kys981207@naver.com>
2023-05-02 12:25:36 +0900

    feat: attraction 기본 구조 추가

---
+++ src/main/java/com/ssafy/attraction/controller/AttractionController.java
@@ -0,0 +1,10 @@
+package com.ssafy.attraction.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/attraction")
+public class AttractionController {
+	
+}
